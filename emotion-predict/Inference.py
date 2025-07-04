fa7d64d52c1d97b5da200c2019dd070079115a1a
chewon1227 <chewon1227@naver.com>
2025-07-04 09:37:30 +0900

    inf

--- emotion-predict/Inference.py
+++ emotion-predict/Inference.py
@@ -1,6 +1,6 @@
 from emotion import EmotionPredictor
 
-predictor = EmotionPredictor("/Users/chaewon/AMO-model-Route-ver.-/DeiT-Tiny_half_best.pth")
-label, intensity, distribution = predictor.predict_from_path("/Users/chaewon/AMO-model-Route-ver.-/disgust.jpg")
+predictor = EmotionPredictor("/Users/chaewon/AMO-model-Route-ver.-/weight/ConvNeXt-Tiny_best.pth") # used model: convnext-tiny 
+label, intensity, distribution = predictor.predict_from_path("/Users/chaewon/AMO-model-Route-ver.-/image-file/smile.png") # put image path ! 
 
-print (label, intensity, distribution)
+print (label, intensity, distribution) 
